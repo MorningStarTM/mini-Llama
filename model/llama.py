cf96ed3d9ce57a766240d525b3dd1f524b3a24c7
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-08-24 09:01:04 +0530

    encoder block created

--- model/llama.py
+++ model/llama.py
@@ -55,6 +55,32 @@ class RMSNorm(nn.Module):
     def forward(self, x:torch.Tensor):
         return self.weight * self._norm(x.float()).type_as(x)
 
+
+class EncoderBlock(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+
+        self.n_heads = args.n_heads
+        self.dim = args.dim
+        self.head_dim = args.dim // args.n_heads
+        self.attention = SelfAttention(args)
+        self.feed_forward = FeedForward(args)
+
+        # Normalization BEFORE the self attention
+        self.attention_norm = RMSNorm(args.dim, eps=args.norm_eps)
+        # Normalization BEFORE the feed forward block
+        self.ffn_norm = RMSNorm(args.dim, eps=args.norm_eps)
+
+    def forward(self, x:torch.Tensor, start_pos: int, freqs_complex: torch.Tensor):
+        #(B, seq_len, Dim) + (B, seq_len, Dim) --> (B, Seq_len, dim)
+        h = x + self.attention.forward(self.attention_norm(x), start_pos, freqs_complex)
+        out = h + self.feed_forward.forward(self.ffn_norm(h))
+        return out
+    
+    
+
+
+
 class Transformer(nn.Module):
     def __init__(self, args:ModelArgs) -> None:
         super().__init__()
